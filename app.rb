4b5a70c1050d49d63aac2d37d4ebd2541f284c92
3ypUP <Posilay.pochashe@gmail.com>
2017-10-16 12:39:00 +0300

    add new erb

--- app.rb
+++ app.rb
@@ -3,6 +3,27 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+
+get '/main' do
+  "Hello World"
+end
+
+
+
+get '/newpost' do
+  "Hello World"
+end
+
+
+
+get '/new' do
+
+erb :new
+
 end
